686a63f99c1431892d2c35111cb2cc1f8420829a
Flora24-Yedidya <69462610+Flora24-Yedidya@users.noreply.github.com>
2025-04-02 11:33:13 +0000

    test7

--- .github/scripts/check_and_deploy.sh
+++ .github/scripts/check_and_deploy.sh
@@ -29,8 +29,7 @@ if [[ -z "$APP_UUID" || "$APP_UUID" == "null" ]]; then
   echo "Lancement de la création de l'application..."
   
   # Appel du script de création d'application
-  pwd && ls -la
-  #./.githubcreate_application.sh "$COOLIFY_URL" "$BRANCH_NAME"
+  ./.github/scripts/create_application.sh "$COOLIFY_URL" "$BRANCH_NAME"
   
   if [[ $? -ne 0 ]]; then
     echo "Erreur lors de la création de l'application."
