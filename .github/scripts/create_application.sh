b9735deaa2750fce1be1f27e557d30cbe4b6106a
Flora24-Yedidya <69462610+Flora24-Yedidya@users.noreply.github.com>
2025-04-02 10:11:35 +0000

    test1

---
+++ .github/scripts/create_application.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PROJECT_UUID=$1
+BRANCH_NAME=$2
+
+# Appel API pour créer l'application
+RESPONSE=$(curl -s -X POST "$COOLIFY_URL/api/v1/applications/private-deploy-key" \
+  --header "Authorization: Bearer $COOLIFY_API_KEY" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "project_uuid": "'"$PROJECT_UUID"'",
+    "server_uuid": "g04w4sg0csocwgocwgkgg44k",
+    "environment_name": "development",
+    "private_key_uuid": "ecokkkckk8cccwgoww00ogg0",
+    "git_repository": "git@github.com:devofs2/pro_erpnext.git",
+    "git_branch": "'"$BRANCH_NAME"'",
+    "ports_exposes": "8081",
+    "build_pack": "dockercompose",
+    "name": "'"$BRANCH_NAME"'",
+    "docker_compose_location": "docker-compose.yml",
+    "docker_compose_custom_build_command": "echo hello | docker login ghcr.io -u devofs2 --password-stdin && docker pull docker pull $IMAGE_NAME "
+    "docker_compose_custom_start_command": "docker compose up -d",
+    "manual_webhook_secret_github": "hello",
+    "instant_deploy": true
+  }')
+
+# Extraire l'UUID de la réponse
+APP_UUID=$(echo "$RESPONSE" | jq -r '.uuid')
+
+# Vérifier si l'UUID est bien récupéré
+if [[ -z "$APP_UUID" || "$APP_UUID" == "null" ]]; then
+  echo "Erreur: Impossible de récupérer l'UUID. Réponse API: $RESPONSE"
+  exit 1
+fi
+
+# Retourner l'UUID
+echo "$APP_UUID"
