463e098f171ec9e868f01da4bd809f5cd9a997c9
Flora24-Yedidya <69462610+Flora24-Yedidya@users.noreply.github.com>
2025-04-03 14:47:22 +0000

    test01

--- .github/scripts/check_and_deploy.sh
+++ .github/scripts/check_and_deploy.sh
@@ -27,6 +27,13 @@ if [[ -z "$RESPONSE" || "$RESPONSE" == "null" ]]; then
   exit 1
 fi
 
+# Vérifier si la requête a réussi
+if [[ -z "$RESPONSE" || "$RESPONSE" == "null" ]]; then
+  echo "Erreur: Impossible de récupérer la liste des applications."
+  echo "Réponse de l'API: $RESPONSE"  # Affiche la réponse brute
+  exit 1
+fi
+
 # Extraire l'UUID de l'application correspondant à la branche
 APP_UUID=$(echo "$RESPONSE" | jq -r --arg BRANCH "$BRANCH_NAME" '.[] | select(.name == $BRANCH) | .uuid')
 
@@ -42,10 +49,9 @@ if [[ -z "$APP_UUID" || "$APP_UUID" == "null" ]]; then
     echo "Erreur: Échec de la création de l'application."
     exit 1
   fi
-
   echo "✅ Application créée avec succès. UUID: $APP_UUID"
 else
   echo "✅ L'application pour la branche '$BRANCH_NAME' existe déjà. UUID: $APP_UUID"
 fi
 
-echo "APP_UUID=$APP_UUID" >> "$GITHUB_ENV"
+# echo "APP_UUID=$APP_UUID" >> "$GITHUB_ENV"
