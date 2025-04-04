a7bc1aab0cde33e21e5c6beb5bffc4805f87ee79
Flora24-Yedidya <69462610+Flora24-Yedidya@users.noreply.github.com>
2025-04-04 08:04:36 +0000

    fix variable values

--- .github/scripts/create_application.sh
+++ .github/scripts/create_application.sh
@@ -18,7 +18,7 @@ RESPONSE=$(curl -s -X POST "$COOLIFY_URL/api/v1/applications/private-deploy-key"
     "build_pack": "dockercompose",
     "name": "'"$BRANCH_NAME"'",
     "docker_compose_location": "docker-compose.yml",
-    "docker_compose_custom_build_command": "echo hello | docker login ghcr.io -u devofs2 --password-stdin && docker pull docker pull ghcr.io/devofs2/pro_erpnext_feature-cicd:latest ",
+    "docker_compose_custom_build_command": "echo $REGISTRY_PASSWORD | docker login ghcr.io -u $GHRC_OWNER --password-stdin && docker pull $IMAGE_NAME",
     "docker_compose_custom_start_command": "docker compose up -d",
     "manual_webhook_secret_github": "hello",
     "instant_deploy": true
