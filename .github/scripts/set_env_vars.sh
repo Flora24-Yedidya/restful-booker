c0ed48c44a8d691fd808317cf570bfb58417f00d
Flora24-Yedidya <69462610+Flora24-Yedidya@users.noreply.github.com>
2025-04-24 06:52:03 +0100

    Update set_env_vars.sh

--- .github/scripts/set_env_vars.sh
+++ .github/scripts/set_env_vars.sh
@@ -1,41 +1,29 @@
 #!/bin/bash
+set -e
 
 APP_UUID=$1
 BRANCH_SANITIZE=$2
 DB_UUID=$3
 
-
 echo "Sending vars to Coolify:"
 echo "BRANCH_SANITIZE: $BRANCH_SANITIZE"
 echo "DB_UUID: $DB_UUID"
 
-JSON_PAYLOAD=$(cat <<EOF
-{
-  "persistentEnvironmentVariables": [
-    { "name": "BRANCH_SANITIZE", "value": "$BRANCH_SANITIZE" },
-    { "name": "DB_UUID", "value": "$DB_UUID" }
-  ]
-}
-EOF
+JSON_PAYLOAD=$(jq -n \
+  --arg bs "$BRANCH_SANITIZE" \
+  --arg db "$DB_UUID" \
+  '{
+    data: [
+      { key: "BRANCH_SANITIZE", value: $bs, is_build_time: true, is_literal: true },
+      { key: "DB_UUID", value: $db, is_build_time: true, is_literal: true }
+    ]
+  }'
 )
 
-echo "JSON payload: $JSON_PAYLOAD"
+echo "JSON payload:"
+echo "$JSON_PAYLOAD"
 
-curl -X PATCH "https://app.coolify.io/api/v1/applications/$APP_UUID" \
+curl -X PATCH "https://app.coolify.io/api/v1/applications/$APP_UUID/envs/bulk" \
   -H "Authorization: Bearer $COOLIFY_API_KEY" \
   -H "Content-Type: application/json" \
   -d "$JSON_PAYLOAD"
-
-
-
-
-
-#curl -X PATCH "https://app.coolify.io/api/v1/applications/$APP_UUID" \
-#  -H "Authorization: Bearer $COOLIFY_API_KEY" \
-#  -H "Content-Type: application/json" \
-#  -d '{
-#    "persistentEnvironmentVariables": [
-#      { "name": "BRANCH_SANITIZE", "value": "'"$BRANCH_SANITIZE"'" },
-#      { "name": "DB_UUID", "value": "'"$DB_UUID"'" }
-#    ]
-#  }'
